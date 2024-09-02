543872c7c758fd866f2d81991f9ad219143582c5
Necollaz <156367647+Necollaz@users.noreply.github.com>
2024-09-02 13:41:46 +0300

    Update Swing.cs

--- Assets/Scripts/Swing.cs
+++ Assets/Scripts/Swing.cs
@@ -7,6 +7,7 @@ public class Swing : MonoBehaviour
     [SerializeField] private float _velocity = 50f;
 
     private HingeJoint _hingeJoint;
+    private KeyCode _actionKey = KeyCode.Space;
 
     private void Awake()
     {
@@ -20,7 +21,7 @@ public class Swing : MonoBehaviour
 
     private void StartMoving()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(_actionKey))
         {
             JointMotor motor = _hingeJoint.motor;
             motor.force = _swingForce;
